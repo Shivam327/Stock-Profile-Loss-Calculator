d2523bc2094603d5317dad8b1d6d398196b3ff82
Shivam <shivamthaker1999@gmail.com>
2021-08-27 22:37:42 +0530

    Edge case Added

--- app.js
+++ app.js
@@ -14,29 +14,33 @@ const submitHandler = () => {
 };
 
 const calculateProfitAndLoss = (buyingPrice, quantity, currPrice) => {
-  if (currPrice < buyingPrice) {
-    let loss = (buyingPrice - currPrice) * quantity;
-    let lossPercentage = (loss / buyingPrice) * 100;
-    if (lossPercentage > 50) {
-      body.style.backgroundColor = "red";
-      outputBox.style.backgroundColor = "red";
-    }
-    showOutput(
-      `It was a bad Day, you made a loss of Rs.${loss} and the loss percent is ${lossPercentage}%`
-    );
-  } else if (currPrice > buyingPrice) {
-    let profit = (currPrice - buyingPrice) * quantity;
-    let profitPercentage = (profit / buyingPrice) * 100;
-    body.style.backgroundColor = "lightgreen";
-    outputBox.style.backgroundColor = "lightgreen";
-
-    showOutput(
-      `Horray, You made the profit of Rs.${profit} and the profit Percent is ${profitPercentage}%`
-    );
+  if (quantity <= 0) {
+    showOutput("Are You mad ??");
   } else {
-    showOutput(
-      `Nither You Lost Anything,Nither You Gained Anything, Wait for Your Right Time`
-    );
+    if (currPrice < buyingPrice) {
+      let loss = (buyingPrice - currPrice) * quantity;
+      let lossPercentage = (loss / buyingPrice) * 100;
+      if (lossPercentage > 50) {
+        body.style.backgroundColor = "red";
+        outputBox.style.backgroundColor = "red";
+      }
+      showOutput(
+        `It was a bad Day, you made a loss of Rs.${loss} and the loss percent is ${lossPercentage}%`
+      );
+    } else if (currPrice > buyingPrice) {
+      let profit = (currPrice - buyingPrice) * quantity;
+      let profitPercentage = (profit / buyingPrice) * 100;
+      body.style.backgroundColor = "lightgreen";
+      outputBox.style.backgroundColor = "lightgreen";
+
+      showOutput(
+        `Horray, You made the profit of Rs.${profit} and the profit Percent is ${profitPercentage}%`
+      );
+    } else {
+      showOutput(
+        `Nither You Lost Anything,Nither You Gained Anything, Wait for Your Right Time`
+      );
+    }
   }
 };
 
