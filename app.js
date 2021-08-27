9f64ceb59c7e9a0fddaf218b180b66d5fc8877ef
Shivam <shivamthaker1999@gmail.com>
2021-08-27 22:30:11 +0530

    Refactored and Bonus Section Aded

--- app.js
+++ app.js
@@ -3,37 +3,45 @@ const stocksQuantity = document.querySelector("#stocks-quantity");
 const currentPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#submit-btn");
 const outputBox = document.querySelector("#output-box");
+const body = document.querySelector("body");
 
-submitBtn.addEventListener("click", submitHandler);
+const submitHandler = () => {
+  let buyingPrice = Number(initialPrice.value);
+  let quantity = Number(stocksQuantity.value);
+  let currPrice = Number(currentPrice.value);
 
-function submitHandler() {
-  let ip = Number(initialPrice.value);
-  let qty = Number(stocksQuantity.value);
-  let curr = Number(currentPrice.value);
+  calculateProfitAndLoss(buyingPrice, quantity, currPrice);
+};
 
-  calculateProfitAndLoss(ip, qty, curr);
-}
-
-function calculateProfitAndLoss(initial, quantity, current) {
-  if (initial > current) {
-    let loss = (initial - current) * quantity;
-    let lossPercentage = (loss / initial) * 100;
-
+const calculateProfitAndLoss = (buyingPrice, quantity, currPrice) => {
+  if (currPrice < buyingPrice) {
+    let loss = (buyingPrice - currPrice) * quantity;
+    let lossPercentage = (loss / buyingPrice) * 100;
+    if (lossPercentage > 50) {
+      body.style.backgroundColor = "red";
+      outputBox.style.backgroundColor = "red";
+    }
     showOutput(
-      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+      `It was a bad Day, you made a loss of Rs.${loss} and the loss percent is ${lossPercentage}%`
     );
-  } else if (current > initial) {
-    let profit = (current - initial) * quantity;
-    let profitPercentage = (profit / initial) * 100;
+  } else if (currPrice > buyingPrice) {
+    let profit = (currPrice - buyingPrice) * quantity;
+    let profitPercentage = (profit / buyingPrice) * 100;
+    body.style.backgroundColor = "lightgreen";
+    outputBox.style.backgroundColor = "lightgreen";
 
     showOutput(
-      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+      `Horray, You made the profit of Rs.${profit} and the profit Percent is ${profitPercentage}%`
     );
   } else {
-    showOutput(`No pain no gain and no gain no pain`);
+    showOutput(
+      `Nither You Lost Anything,Nither You Gained Anything, Wait for Your Right Time`
+    );
   }
-}
+};
 
 function showOutput(message) {
   outputBox.innerHTML = message;
 }
+
+submitBtn.addEventListener("click", submitHandler);
